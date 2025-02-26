01a5b65bc3362916766060a11fe40e3055f2ce1c
MariaNand4 <168575583+NandaMariaa@users.noreply.github.com>
2025-02-26 14:29:20 -0300

    ex2

--- ex2/main.py
+++ ex2/main.py
@@ -1,3 +1,8 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
+p1 = Pessoa('Luiz', 29)
+
+p1.comer('Maça')
+p1.comer('Maça')
+p1.parar_comer()
+p1.parar_comer()
--- ex2/pessoa.py
+++ ex2/pessoa.py
@@ -1,3 +1,43 @@
 class Pessoa:
-    def __init__(self, nome, idade, comendo=False, falando=False):
-        pass 
+    def __init__(self, nome, idade, comendo = False, falando = False):
+
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    # Comer.
+    def comer (self, alimento):
+
+        if self.comendo:
+            print (f'{self.nome} já esta comendo: {alimento}')
+            return
+
+        print (f'{self.nome} esta comendo: {alimento}')   
+        self.comendo = True
+
+    # Parar de comer.
+    def parar_comer(self):
+
+        if not self.comendo:
+            print (f'{self.nome} não está comendo.')
+            return
+        print (f'{self.nome} parou de comer.')
+        self.comendo = False
+
+    # Falar.
+    def falar (self, assunto):
+
+        if self.comendo:
+            print(f'{self.nome} não consegue falar enquanto está comendo.')
+            return
+        
+        if self.falando:
+            print (f'{self.nome} já esta conversando.')
+            return
+        
+        print (f'{self.nome} começou a falar.')
+        self.falando = True
+        
+
+
